594eb3d15d9541e5c35bf21df87c7da456b67375
joseramirez <e.josem91@gmail.com>
2019-11-26 11:38:30 -0600

    Create variables for content cargo carried and new function for getting weight in tonnes

--- src/U1.kt
+++ src/U1.kt
@@ -1,14 +1,21 @@
+import java.util.*
+
 class U1 :Rocket() {
+   private  var maxWeightTonnes:Int =18
+   private  var vargoCarried: Int = 100000
+
     override fun launch(): Boolean {
-
-
-
-        return super.launch()
+        val chaceExplosion = 0.05 *(vargoCarried/tonneToKgm(maxWeightTonnes))
+        val ramdomCreated = Random()
+        val ramdom = ramdomCreated.nextDouble()
+        return ramdom > chaceExplosion
     }
 
     override fun land(): Boolean {
-
-
         return super.land()
+    }
+
+    private fun tonneToKgm(maxWeight: Int): Int{
+        return maxWeight * 1000
     }
 }
